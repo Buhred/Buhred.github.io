3a29d1f1a576680e504c8b08851ca6897831bab6
Buhred <318357857+Buhred@users.noreply.github.com>
2026-08-18 23:14:05 +0300

    Website is sooo up bru 🤑🤑‼️‼️‼️
    I fucked up something so had to fix

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Buhred.github.io
-Websiteing it
+# The biolink thingy
+
+I genuinely have zero idea as to how reading this README or even being in this repository could help you in the slightest. It is pretty self explanatory, ain't it?
+
+![youtuber](Memes/youtuber.webp)
---
+++ script.js
@@ -0,0 +1,11 @@
+const Birthday = new Date(2008, 11, 19);
+
+const Today = new Date();
+
+let Age = Today.getFullYear() - Birthday.getFullYear();
+
+if (Today.getMonth() < Birthday.getMonth() || (Today.getMonth() === Birthday.getMonth() && Today.getDate() < Birthday.getDate())) {
+	Age--;
+}
+
+document.getElementById("Age").textContent = Age;
